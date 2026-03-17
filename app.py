7b978498ce5a6d4638e0fd1f54c4f98448e83a51
statalite <kimse@karmanos.org>
2026-03-17 16:36:48 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4030,14 +4030,14 @@ ui.div(
     ui.p(ui.strong("▷ Toy Datasets for the Shiny App"),
          style="margin-top:20px; margin-bottom:2px"),
 
-    ui.p(
-        ui.HTML(
-            "Example datasets for testing the Shiny interface are located in the "
-            "<a href="https://github.com/hdlugas/pycompound/tree/main/tests/data" target="_blank"><code>tests/data/</code></a> directory of the PyCompound GitHub repository. "
-            "The appropriate files depend on the selected platform and analysis option."
-        ),
-        style="font-size:15px; margin-top:0px; margin-bottom:8px"
+ui.p(
+    ui.HTML(
+        "Example datasets for testing the Shiny interface are located in the "
+        "<a href='https://github.com/hdlugas/pycompound/tree/main/tests/data' target='_blank'><code>tests/data/</code></a> directory of the PyCompound GitHub repository. "
+        "The appropriate files depend on the selected platform and analysis option."
     ),
+    style="font-size:15px; margin-top:0px; margin-bottom:8px"
+),
 
     ui.HTML(
     """
